6027189c6408a9997855505f919b23a902e12be4
Aditi020 <92170627+Aditi020@users.noreply.github.com>
2023-11-23 12:54:13 +0000

    Added Dynamic data.

--- app.py
+++ app.py
@@ -1,7 +1,60 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, jsonify
 app=Flask(__name__)
+
+JOBS=[
+{
+  'id':1,
+  'title':"Data Analyst",
+  'location':'Benguluru,India',
+  'salary':'Rs 10,00,000'
+},
+  {
+    'id':2,
+    'title':"Data Science",
+    'location':'Delhi,India',
+    'salary':'Rs 15,00,000'
+  },
+  {
+    'id':3,
+    'title':"Software Engineer",
+    'location':'Remote',
+    'salary':'Rs 18,00,000'
+  },
+{
+  'id':4,
+  'title':"Devops Engineer",
+  'location':'Haryana,India',
+  'salary':'Rs 22,00,000'
+},
+{
+  'id':5,
+  'title':"Frontend Engineer",
+  'location':'San Francisco',
+},
+{
+  'id':6,
+  'title':"Backend Engineer",
+  'location':'Disney Land',
+  'salary':'$ 12,000'
+}
+]
+
 @app.route("/")
-def hellow_world():
-  return render_template('home.html')
+def hello_world():
+  return render_template('home.html', Jobs=JOBS , company_name="Jarvis")
+ 
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True)  
+  app.run(host='0.0.0.0',debug=True)
+
+
+
+
+    
+
